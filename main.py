b46e1583f833599e333ca6711272801d209836dc
Timothy Holmes <tim.a.holmes@gmail.com>
2022-08-03 14:09:36 +1000

    added bytes decoder and fixed return None error

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ from zoneinfo import ZoneInfo
 from datetime import datetime
 tzinfo = ZoneInfo("Australia/Melbourne")
 
+class BytesEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            return obj.decode('utf-8')
+        return json.JSONEncoder.default(self, obj)
+
 async def app(scope, receive, send):
     datetime_str = datetime.now(tz=tzinfo).strftime('%Y%m%d%H%M%S')
     path = f'/data/request-{datetime_str}.json'
+    # path = f'C:\\Code\\home-server\\request-logger\\data\\request-{datetime_str}.json'
+    print(scope)
     with open(path,'w') as json_record:
-        json.dump(scope,json_record,indent=4)
-    return 'Request received'
+        json.dump(scope,json_record,indent=4,cls=BytesEncoder)
+    return None # ASGI callable should return None
 
 if __name__ == "__main__":
     uvicorn.run(
