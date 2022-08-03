08e0d1601e73a7ec7d2d5ac04779b32c96941263
Timothy Holmes <tim.a.holmes@gmail.com>
2022-08-03 11:57:06 +1000

    initial commit - this might work

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # request-logger
-Records requests to FastAPI server. Opportunity to turn this into middleware for hosted web apps to assist with debugging
+Records requests to server. Opportunity to turn this into middleware for hosted web apps to assist with debugging.
---
+++ main.py
@@ -0,0 +1,20 @@
+import uvicorn
+import json
+from zoneinfo import ZoneInfo
+from datetime import datetime
+tzinfo = ZoneInfo("Australia/Melbourne")
+
+async def app(scope, receive, send):
+    datetime_str = datetime.now(tz=tzinfo).strftime('%Y%m%d%H%M%S')
+    path = f'/data/request-{datetime_str}.json'
+    with open(path,'w') as json_record:
+        json.dump(json_record,indent=4)
+    return 'Request received'
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app", 
+        host = '192.168.1.103', 
+        port=12345, 
+        log_level="info"
+    )
