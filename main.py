3aea512a4aa3ccff402d217acbe870d560222fdb
Timothy Holmes <tim.a.holmes@gmail.com>
2022-08-03 13:40:17 +1000

    added host address to uvicorn.run: 0.0.0.0

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def app(scope, receive, send):
 if __name__ == "__main__":
     uvicorn.run(
         "main:app", 
-        # host = '192.168.1.103', 
+        host = '0.0.0.0', 
         port=12345, 
         log_level="info"
     )
